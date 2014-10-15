1892a49fbd51d48ea0fd11034b9c81cf87f041b5
sa3047 <swapnil_aher@yahoo.co.in>
2014-10-14 22:09:07 -0700

    client and server code changed

--- hm_server.py
+++ hm_server.py
@@ -6,7 +6,9 @@ tcpSerSock = socket(AF_INET, SOCK_STREAM)
 
 # Prepare a server socket
 tcpSerSock.bind(('192.168.56.1' , tcpSerPort))
-tcpSerSock.listen(1)
+
+#This will receive 10 clients connection
+tcpSerSock.listen(10)
 
 while True:
     # Start receiving data from the client
@@ -15,19 +17,25 @@ while True:
     print 'Received a connection from: ', addr
 	
     try:
-		message = tcpCliSock.recv(1024)
+		message = tcpCliSock.recv(4096)
 		
 		print 'message split '+ message
 		
 		filename = message.split()[1]
-		
+		print 'file name  -> '+ filename[1:]
 		f = open(filename[1:])
 		outputdata = f.read()
         
+		print 'output data '+ outputdata
+		
+		tcpCliSock.send(outputdata)
+		
+		'''for i in range(0, len(outputdata)):
+			tcpCliSock.send(outputdata[i])'''
+		
 		tcpCliSock.send("http/1.1 200 OK\r\n\r\n") #\r\n
-        
-		for i in range(0, len(outputdata)):
-			tcpCliSock.send(outputdata[i])
+
+		
 			
 		print 'Request Completed'
 		
@@ -35,8 +43,8 @@ while True:
 		
     except IOError:
         #Send response message for file not found
-        tcpCliSock.send('404: File Not Found')
+		tcpCliSock.send('404: File Not Found')
         #Close client socket
-        tcpCliSock.close()   
+		tcpCliSock.close()   
 
 tcpSerSock.close()
--- hw_client.py
+++ hw_client.py
@@ -10,8 +10,8 @@ except socket.error, msg:
  
 print 'Socket Created'
  
-host = '192.168.56.1'
-port = 1080
+host = str(sys.argv[1]) #'192.168.56.1'
+port = int(sys.argv[2]) #1080
  
 try:
     remote_ip = socket.gethostbyname( host )
@@ -29,8 +29,8 @@ s.connect((remote_ip , port))
 print 'Socket Connected to ' + host + ' on ip ' + remote_ip
  
 #Send some data to remote server
-#message = "GET helloworld.html HTTP/1.1\r\n\r\n"
-message = "GET /helloworld.html HTTP/1.1" 
+message = "GET /helloworld.html HTTP/1.1\r\n\r\n"
+#message = "GET /helloworld.html HTTP/1.1" 
 try :
     #Set the whole string
     s.sendall(message)
@@ -45,7 +45,8 @@ reply = s.recv(4096)
  
 print 'Reply from python server  ' + reply
 
-for i in range(0, len(reply)):
-	print(reply[i])
+
+'''for i in range(0, len(reply)):
+	print(reply[i])'''
 	
 s.close()
